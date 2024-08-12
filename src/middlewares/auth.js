1be06ee0b8e5e7425c2cbaf779e374938dd86188
Erick Vera <erickverac90@gmail.com>
2024-08-11 20:33:37 -0600

    Agregando endpoints post get patch y delete en posts

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,39 +1,27 @@
-const createError = require("http-errors");
+const jwt = require("../lib/jwt");
+const User = require("../models/user.model");
 
-const userUseCases = require("../usecases/users.usecase");
+async function auth(req, res, next) {
+  try {
+    const authorization = req.headers.authorization;
 
-const jwt = require("jsonwebtoken");
+    const token = authorization?.replace("Bearer ", "");
 
-function auth() {
-  return async (req, res, next) => {
-    try {
-      const authorization = req.headers.authorization;
+    const decoded = await jwt.verify(token);
+    const user = await User.findById(decoded.id);
 
-      const token = authorization?.replace("Bearer ", "");
-
-      if (!token) {
-        throw createError(401, "Token is required");
-      }
-
-      const payload = jwt.verify(token);
-
-      const user = await userUseCases.getById(payload.id);
-
-      if (!user) {
-        throw createError(401, "Unauthorized");
-      }
-
-      req.user = user;
-
-      next();
-    } catch (error) {
-      res.status(error.status || 500);
-      res.json({
-        success: false,
-        message: error.message,
-      });
+    if (!user) {
+      throw createError(401, "Unauthorized");
     }
-  };
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Unauthorized",
+    });
+  }
 }
 
 module.exports = auth;
